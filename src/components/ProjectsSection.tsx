7df98922193d655f4f1fadaa80eda35ee0aac12e
Eduardo Barbosa Silva <eduardobarbosasilva.oficial@outlook.com>
2025-11-08 20:12:58 -0300

    feat: ajuste no link de projeto

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -6,7 +6,7 @@ const projects = [
         title: "Rezap Planejamento",
         description: "Uma página de planejamento e vizualização de tarefas",
         image: "/projects/rezap.png",
-        tags: ["React", "TypeScript", "HTML", "CSS"],
+        tags: ["React", "TypeScript", "HTML", "CSS", "C#", "ASP.NET"],
         demoUrl: "https://dashboard-planejamento-63pi0wsav-xcode-sketchers-projects.vercel.app/",
         github: "https://github.com/Xcode-sketcher/Dashboard-planejamento"
     },
@@ -43,13 +43,13 @@ export const ProjectSection = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {projects.map((project, key) => (
-                    <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                    <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover flex flex-col h-full">
 
                         <div className="h-48 overflow-hidden">
                             <img src={project.image} alt={project.title} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
                         </div>
 
-                        <div className="p-6">
+                        <div className="p-6 flex flex-col flex-grow">
                             <div className="flex flex-wrap gap-2 mb-4">
                                 {project.tags.map((tag) => (
                                     <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
@@ -61,7 +61,7 @@ export const ProjectSection = () => {
 
                             <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
                             <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
-                            <div className="flex justify-between items-center">
+                            <div className="flex justify-between items-center mt-auto">
                                 <div className="flex space-x-3">
                                     <a href={project.demoUrl} className="text-foreground/80 hover:text-primary transition-colors duration-300" target="_blank">
                                         <ExternalLinkIcon size={20} />
